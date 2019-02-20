ebec00eaa5d83b0cfd341ccd71634ad66d700bcf
Marcus Peterson <mdabomb5@gmail.com>
2019-02-19 17:00:41 -0700

    added local storage

--- src/Houses.js
+++ src/Houses.js
@@ -15,30 +15,58 @@ class Houses extends Component {
         }
     }
 
+    componentDidMount() {
+        const gPoints = localStorage.getItem('gPoints');
+        const hPoints = localStorage.getItem('hPoints');
+        const sPoints = localStorage.getItem('sPoints');
+        const rPoints = localStorage.getItem('rPoints');
+        if (gPoints) {
+            this.setState({ gPoints: JSON.parse(gPoints) });
+        }
+        if (hPoints) {
+            this.setState({ hPoints: JSON.parse(hPoints) });
+        }
+        if (sPoints) {
+            this.setState({ sPoints: JSON.parse(sPoints) });
+        }
+        if (rPoints) {
+            this.setState({ rPoints: JSON.parse(rPoints) });
+        }
+
+    }
+
     add = (num, house) => {
         if (house === "Gryffindor"){
             this.setState(prevState => {
                 return {
                     gPoints: prevState.gPoints + num
                 }
+            },() => {
+                localStorage.setItem('gPoints', JSON.stringify(this.state.gPoints) )
             })
         }else if (house === "Hufflepuff"){
             this.setState(prevState => {
                 return {
                     hPoints: prevState.hPoints + num
                 }
-            })
+            },() => {
+                localStorage.setItem('hPoints', JSON.stringify(this.state.hPoints) )
+            }) 
         }else if (house === "Slytherin"){
             this.setState(prevState => {
                 return {
                     sPoints: prevState.sPoints + num
                 }
+            },() => {
+                localStorage.setItem('sPoints', JSON.stringify(this.state.sPoints) )
             })
         }else if (house === "Ravenclaw"){
             this.setState(prevState => {
                 return {
                     rPoints: prevState.rPoints + num
                 }
+            },() => {
+                localStorage.setItem('rPoints', JSON.stringify(this.state.rPoints) )
             })
         }
     }
